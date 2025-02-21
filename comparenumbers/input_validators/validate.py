6ff230c94a8c9f95fc1df36a8cc5a1875416b31a
nalm2812 <nalm@itu.dk>
2025-02-21 14:18:54 +0100

    test

--- comparenumbers/input_validators/validate.py
+++ comparenumbers/input_validators/validate.py
@@ -5,13 +5,16 @@ import re
 for _ in range(2):
     line = sys.stdin.readline() #Reads the line
     if not re.match(r"(0|([1-9][0-9]*))\n", line): #Checks for actual numbers
+        print("first")
         sys.exit(43) #fail
     try:
         x = int(line) #Turns the line(string) into an int
 
         if not 0 <= x <= 100: #Checks the range
+            print("second")
             sys.exit(43) #fail
     except ValueError:
+        print("third")
         sys.exit(43) #fail
 
 if sys.stdin.readline() != "": #Checks that there are only the desired amount of inputs
