d8c50222b77f86fda2ae7331c011e501d3cf3912
Adrian <adrianjuul123@gmail.com>
2025-02-21 13:39:31 +0100

    sleepy

--- compareNumbers/submissions/time_limit_exceeded/sleep.py
+++ compareNumbers/submissions/time_limit_exceeded/sleep.py
@@ -1,8 +1,15 @@
 import time
 
+
 a = int(input())
 b = int(input())
-c = int(input())
+
 for _ in range(10**10):
     a = a + b - b
-print(*sorted((a,b,c)))
+
+if a == b:
+    print("equal")
+elif a < b:
+    print("smaller")
+elif a > b:
+    print("bigger")
